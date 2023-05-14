a2bac2a76bac574926d5a6783fbf0ee22b0bdec5
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-05-14 15:28:21 +0100

    update html to handlebars

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const fs = require('fs');
 const path = require('path');
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create();
 
 //routes
 //const apiRoutes = require('./routes/apiroutes');
@@ -10,6 +12,9 @@ const path = require('path');
 //server setup
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 
 app.listen(port, () => {
